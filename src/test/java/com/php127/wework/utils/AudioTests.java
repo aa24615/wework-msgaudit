669810ea69c03192fe358f2ce51bfbb97d34fff1
读心印 <839024615@qq.com>
2022-01-11 18:29:02 +0800

    dev

--- src/test/java/com/php127/wework/utils/AudioTests.java
+++ src/test/java/com/php127/wework/utils/AudioTests.java
@@ -1,16 +1,16 @@
 package com.php127.wework.utils;
 
-
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class AudioTests {
 
     @Test
-    public void testToMp3() throws Exception {
-        Assert.assertEquals("zhihao.miao","zhihao.miao");
+    public void testToMp3()  throws Exception {
+        System.out.println("黑黑");
+//        Assert.assertEquals("zhihao.miao","zhihao.miao");
     }
-
-
 }
